d9e58b962ae8c878a4c1c1144e1dbd53354ee071
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2022-01-06 13:44:01 +0100

    push

--- interface/src/api/src/data.ts
+++ interface/src/api/src/data.ts
@@ -1,7 +1,7 @@
 import { Product } from './classes/product';
 
 export const PRODUCTS: Product[] = [
-    new Product(1, 'Pomme', 0.50, 'http://assets.stickpng.com/images/580b57fbd9996e24bc43c11c.png'),
+    new Product(1, 'Pomme', 0.50, 'https://www.coteaux-nantais.com/sites/coteaux-nantais.com/files/pilot.png'),
     new Product(2, 'Poire', 0.74, 'http://assets.stickpng.com/images/580b57fcd9996e24bc43c17d.png'),
     new Product(3, 'Banane', 0.35, 'https://www.lifepng.com/wp-content/uploads/2020/11/Isolated-Banana-png-hd.png'),
     new Product(4, 'Visseuse', 59.99, 'https://media.adeo.com/marketplace/LMFR/82253643/ffc92534-51f0-4b98-b266-e0122ba8a4d4.png'),
