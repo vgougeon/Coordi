1bca50e7e812e2d263eaf465a03fb9f6672909df
Vicnent GOUGEON <gougeonvincent31@gmail.com>
2022-01-13 13:36:47 +0100

    unknown product

--- interface/src/components/order.tsx
+++ interface/src/components/order.tsx
@@ -10,6 +10,7 @@ import Header from "../layout/header"
 import { useEffect } from "react"
 import { useNavigate } from "react-router"
 import WaitPayAmount from "./waitPayAmount"
+import UnknownProductModal from "./unknownProductModal"
 export default function Order({ id }: { id: number }) {
     const currentState = useObservable(orderService.getOrder(id).currentState$)
     const state: State = useObservable(orderService.getOrder(id).state$)
@@ -38,11 +39,7 @@ export default function Order({ id }: { id: number }) {
                                 {(currentState === 'WAIT_QUANTITY' || currentState === 'WAIT_FOR_RETURN_QUANTITY') && <Quantity id={id} />}
                                 {(currentState === 'PAY') && <PayComponent id={id} />}
                                 {(currentState === 'PAY_WAIT_AMOUNT') && <WaitPayAmount id={id} />}
-                                {(currentState === 'UNKNOWN_PRODUCT') && <div className="p-4 flex flex-col items-center">
-                                    <h2 className="text-lg">Produit inconnu</h2>
-                                    <OkButton id={id} />
-                                </div>}
-
+                                {(currentState === 'UNKNOWN_PRODUCT') && <UnknownProductModal id={id} />}
                             </div>
                             {(currentState === 'ORDER_FINISH') && <div className="p-4 flex flex-col items-center">
                                 <h2 className="text-lg">Paiement validé.</h2>
