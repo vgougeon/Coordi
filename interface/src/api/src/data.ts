be0011a5ef29af726aa80defd4b0d467e3fc8d83
thomas <thomas.alzonne@gmail.com>
2022-01-13 13:57:24 +0100

    feat de mega turbo dep omg

--- interface/src/api/src/data.ts
+++ interface/src/api/src/data.ts
@@ -1,15 +1,15 @@
 import { Product } from './classes/product';
 
 export const PRODUCTS: Product[] = [
-    new Product(1, 'Visseuse', 'Machine', 24.9, 'https://media.adeo.com/media/1664469/format/png?format=jpg&width=700'),
-    new Product(2, 'Perceuse', 'Machine', 41.9, 'https://media.adeo.com/media/1664515/format/png?format=jpg&width=700'),
-    new Product(3, 'Perforateur','Machine', 129.0, 'https://media.adeo.com/media/1664470/format/png?format=jpg&width=700'),
+    new Product(1, 'Visseuse', 'Jardin', 24.9, 'https://media.adeo.com/media/1664469/format/png?format=jpg&width=700'),
+    new Product(2, 'Perceuse', 'Outillage', 41.9, 'https://media.adeo.com/media/1664515/format/png?format=jpg&width=700'),
+    new Product(3, 'Perforateur','Outillage', 129.0, 'https://media.adeo.com/media/1664470/format/png?format=jpg&width=700'),
     new Product(4, 'Ponceuse','Machine', 79.99, 'https://media.adeo.com/media/1664516/format/png?format=jpg&width=700'),
-    new Product(5, 'Polisseuse','Vincent', 69.99, 'https://media.adeo.com/media/1664517/format/png?format=jpg&width=700'),
+    new Product(5, 'Polisseuse','Machine', 69.99, 'https://media.adeo.com/media/1664517/format/png?format=jpg&width=700'),
     new Product(
       6,
       'Foret et mèche',
-      'FDP',
+      'Embout',
       15.5,
       'https://m1.lmcdn.fr/media/15/5eea2473475ea72daca3dd10_original/3265160300/foret-et-meche5eea2473dab3a5000976602f.jpg?format=jpg&width=700'
     ),
@@ -23,7 +23,7 @@ export const PRODUCTS: Product[] = [
     new Product(
       8,
       'Tronçonneuse à métaux',
-      'TG',
+      'Machine',
       279.0,
       'https://m1.lmcdn.fr/media/15/5d7618803711266606846c65/357545486/famille-tronconneuse-a-metaux5d7618800ab56b00097c23b5.jpg?format=jpg&width=700'
     ),
